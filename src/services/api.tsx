cff0755d7758b5b95c8108d23461917d2c450e25
Dario G <dariogabalec@gmail.com>
2020-07-29 00:58:11 -0300

    url API updated to https

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import { movieType } from "../pages/Home";
 
 const apiKey = "26277d7a";
+const baseUrl = "https://www.omdbapi.com/?apikey="
 
 export const getMovies = async (movieName: string): Promise<movieType[]> => {
   return await axios
-    .get(`http://www.omdbapi.com/?apikey=${apiKey}&s=${movieName}`)
+    .get(`${baseUrl}${apiKey}&s=${movieName}`)
     .then((response) => {
       // handle success
       return response.data.Error !== undefined
@@ -24,7 +25,7 @@ export const getMovies = async (movieName: string): Promise<movieType[]> => {
 
 export const getMovieDetails = async (imdbID: string): Promise<movieType> => {
   return await axios
-    .get(`http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`)
+    .get(`${baseUrl}${apiKey}&i=${imdbID}`)
     .then((response) => {
       // handle success
       if (response.data.Error!) {
