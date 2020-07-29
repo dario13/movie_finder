cd50554a07e49cdb0801e6b06a51f8baf352885c
Dario G <dariogabalec@gmail.com>
2020-07-28 23:09:11 -0300

    Updates

---
+++ precache-manifest.85dbf7e58ae79722c6f8d8ddb7481dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6576e60eb735e874bc15f14d4347bd45",
+    "url": "/movie_finder/index.html"
+  },
+  {
+    "revision": "497a9531e27cb92245cc",
+    "url": "/movie_finder/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "7eada9ebe916d5f0f7b9",
+    "url": "/movie_finder/static/css/main.96db9fdd.chunk.css"
+  },
+  {
+    "revision": "497a9531e27cb92245cc",
+    "url": "/movie_finder/static/js/2.d299d7fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_finder/static/js/2.d299d7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eada9ebe916d5f0f7b9",
+    "url": "/movie_finder/static/js/main.68b858c1.chunk.js"
+  },
+  {
+    "revision": "e815237217493651c852",
+    "url": "/movie_finder/static/js/runtime-main.23b5bf38.js"
+  }
+]);
